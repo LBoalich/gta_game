701e3ee9d163be9360350a8d19ed7389abc7d2a5
LBoalich <lkboalich@gmail.com>
2023-03-08 19:07:57 -0500

    Add buy laptop method to Player class

--- Player.py
+++ Player.py
@@ -1,4 +1,3 @@
-#create player class
 class Player:
     def __init__(self, name, driving_ability, hacking_ability, money_total):
         self.name = name
@@ -7,7 +6,6 @@ class Player:
         self.money_total = money_total
         self.laptops = []
 
-    #player description
     def __repr__(self):
         a_an = ''
         current_laptops = ''
@@ -23,4 +21,41 @@ class Player:
             current_laptops = ', '.join(self.laptops)
 
         return "My name is {name}.  I am {a_or_an} {hacking_ability} level hacker.  I am {driving_ability} at driving a car.  I have ${money_total} and am currently carrying the following laptops: {laptops}.".format(name = self.name, hacking_ability = self.hacking_ability, driving_ability = self.driving_ability, a_or_an = a_an, money_total = self.money_total, laptops = current_laptops)
+    
+    def buy_laptop(self):
+        laptop_list = [['green', 100], ['blue', 300], ['black', 500], ['red', 1000], ['gold', 5000]]
+        laptop_string = 'The laptop prices are: '
+        laptop_color_string = ''
+        laptop_color_list = []
+
+        for laptop in laptop_list:
+            if laptop[0] in self.laptops:
+                pass
+            else:
+                laptop_string += ('{laptop_color} ${laptop_price}, '.format(laptop_color = laptop[0], laptop_price = str(laptop[1])))
+                laptop_color_string += (laptop[0] + ' ')
+                laptop_color_list.append(laptop[0])
+
+        print('You have ${amount}.'.format(amount = self.money_total))
+        print(laptop_string)
+
+        laptop_color = input('Which color laptop do you want to buy? ').lower()
+        
+        while laptop_color not in laptop_color_list:
+            laptop_color = input('Looks like you had a typo.  Please re-enter the color laptop you want to purchase: ').lower()
+
+        for laptops in laptop_list:
+            if laptop_color == laptops[0]:
+                laptop_price = laptops[1]
+
+        if self.money_total < laptop_price:
+            print("You do not have enough money to buy that laptop.  Sadge.  Pick a different laptop sucker!")
+            self.buy_laptop()
+        else:
+            self.money_total -= laptop_price
+            self.laptops.append(laptop_color)
+            print('You spent ${laptop_price} and now have ${money_total} left.'.format(laptop_price = laptop_price, money_total = self.money_total))
+            return laptop_color
+
+
 
--- gta_game.py
+++ gta_game.py
@@ -1,8 +1,9 @@
 from Player import Player
 
 #create Buddha, Tony, Yuno
-lang_buddha = Player('Lang Buddha', 'bad', 'average', 500)
+lang_buddha = Player('Lang Buddha', 'bad', 'average', 5000)
 tony_corleone = Player('Tony Corleone', 'great', 'beginner', 500)
 yuno_sykk = Player('Yuno Sykk', 'good', 'exellent', 500)
+
 
 
